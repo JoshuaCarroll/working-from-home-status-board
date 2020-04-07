2e9293e8100afa30ff4b219e62bc9f1601b4e8e4
Joshua Carroll <joshcarroll21@gmail.com>
2020-04-07 15:29:35 -0500

    Added external stylesheet

--- status.php
+++ status.php
@@ -17,64 +17,7 @@
 	<head>
 		<title>Status</title>
 		<script src="jquery.min.js"></script>
-		<style>
-			/* carter-one-regular - latin */
-			@font-face {
-			  font-family: 'Carter One';
-			  font-style: normal;
-			  font-weight: 400;
-			  src: local('Carter One'), local('CarterOne'),
-				   url('fonts/carter-one-v11-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-				   url('fonts/carter-one-v11-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-			}
-			
-			body {
-				font-family: 'Carter One', cursive;
-			}
-
-			.body {
-				display: block;
-				top: 0px;
-				bottom: 0px;
-				left: 0px;
-				position: absolute;
-				width: 100%;
-			}
-
-			#both-aligned {
-				left: 50%;
-				position: absolute;
-				top: 50%;
-				-webkit-transform: translateX(-50%) translateY(-50%);
-				transform: translateX(-50%) translateY(-50%);
-				text-align: center;
-			}
-			
-			select {
-				font-size: 50px;
-			}
-			
-			input {
-				font-size: 25px;
-			}
-			
-			.available {
-				
-				color: #006600;
-				background: #000;
-			}
-			.inMeeting {
-				
-				color: #fff;
-				background: #660000;
-			}
-			.recording {
-				
-				color: #fff;
-				background: #bb4800;
-			}
-
-		</style>
+		<link rel="stylesheet" href="style.css" type="text/css">
 		<script type="text/javascript">
 			function setStatus(status) {
 				$(".body").removeClass("available");
@@ -89,7 +32,7 @@
 			<div id="both-aligned">
 				<span>
 					<form action="" method="GET">
-						<select name="status" id="status">
+						<select name="status" id="status" onchange="this.form.submit()">
 							<option value="available">Available</option>
 							<option value="inMeeting">In a meeting</option>
 							<option value="recording">Recording</option>
